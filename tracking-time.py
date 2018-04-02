f8e9264a1175e8e2b28714fbf1af9abab8181749
David Darke <david@atomicsmash.co.uk>
2018-04-02 15:33:49 +0100

    Merged in POST request into a button push demo

--- tracking-time.py
+++ tracking-time.py
@@ -1,33 +1,57 @@
 import config
 import requests
 import datetime
+import RPi.GPIO as GPIO
+import time
 
-# Get current date and time
-now = datetime.datetime.now()
+GPIO.setmode(GPIO.BCM)
 
-print "GET all of the available tasks:"
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO23
+GPIO.setup(24, GPIO.OUT) #LED to GPIO24
+
+try:
+    while True:
+        button_state = GPIO.input(23)
+        if button_state == False:
+
+            GPIO.output(24, True)
+            print('Button Pressed...')
+
+            print "Now fire a POST request to start tracking a task"
+
+            # Get current date and time
+            now = datetime.datetime.now()
+
+            # Fire a POST request to start tracking a task
+            r = requests.post(
+                "https://app.trackingtime.co/api/v4/tasks/track/" + config.TT_CONFIG['taskid'],
+                data = {'date' : now.strftime("%Y-%m-%d %H:%M:%S") },
+                auth=( config.TT_CONFIG['username'] , config.TT_CONFIG['password'] )
+            )
+
+            # Output a data from the request
+            print r.status_code
+            print r.reason
+            print r.content
+
+            time.sleep(0.3)
+
+        else:
+            GPIO.output(24, False)
+except:
+    GPIO.cleanup()
+
+
+
+# print "GET all of the available tasks:"
 
 # Fire a GET request to pull all of the user's available tasks
-r = requests.get(
-    "https://app.trackingtime.co/api/v4/users/" + config.TT_CONFIG['userid'] + "/tasks/",
-    auth=( config.TT_CONFIG['username'] , config.TT_CONFIG['password'] )
-)
-
-# Output a data from the request
-print r.status_code
-print r.reason
-print r.content
-
-print "Now fire a POST request to start tracking a task"
-
-# Fire a POST request to start tracking a task
-r = requests.post(
-    "https://app.trackingtime.co/api/v4/tasks/track/" + config.TT_CONFIG['taskid'],
-    data = {'date' : now.strftime("%Y-%m-%d %H:%M:%S") },
-    auth=( config.TT_CONFIG['username'] , config.TT_CONFIG['password'] )
-)
-
-# Output a data from the request
-print r.status_code
-print r.reason
-print r.content
+# r = requests.get(
+#     "https://app.trackingtime.co/api/v4/users/" + config.TT_CONFIG['userid'] + "/tasks/",
+#     auth=( config.TT_CONFIG['username'] , config.TT_CONFIG['password'] )
+# )
+#
+# # Output a data from the request
+# print r.status_code
+# print r.reason
+# print r.content
